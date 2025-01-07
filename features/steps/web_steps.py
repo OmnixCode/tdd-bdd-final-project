2e98b254487980e367091763898a50fd29107db3
OmnixCode <kilibarda@gmail.com>
2025-01-07 11:35:43 -0500

    Added all the steps

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -126,6 +126,15 @@ def step_impl(context, name):
     element = context.driver.find_element(By.ID,'search_results')
     assert(name not in element.text)
 
+@then(u'I should see the message "{message}"')
+def step_impl(context, message):
+    found = WebDriverWait(context.driver, context.wait_seconds).until(
+        expected_conditions.text_to_be_present_in_element(
+            (By.ID,'flash_message'),
+            message
+        )
+    )
+    assert(found)
     
 
 ##################################################################
